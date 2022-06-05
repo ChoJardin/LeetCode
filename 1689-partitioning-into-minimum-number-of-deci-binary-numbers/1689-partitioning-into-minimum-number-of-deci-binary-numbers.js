72d7ae9a4779382376896e758e4f479e0eaa904d
조정원 <77387798+ChoJardin@users.noreply.github.com>
2022-06-05 22:17:44 +0900

    Time: 101 ms (77.54%), Space: 46.9 MB (62.46%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} n
+ * @return {number}
+ */
+var minPartitions = function(n) {
+    
+    let max = 0;
+    
+    for (let i=0; i < n.length; i++) {
+        if (+n[i] > max) {
+            max = +n[i];
+        }
+    }
+    
+    return max;
+};
