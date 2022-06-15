ae38406b1c9402b7cfece58c5479b6287376a0a2
조정원 <77387798+ChoJardin@users.noreply.github.com>
2022-06-15 17:29:56 +0900

    Time: 78 ms (65.91%), Space: 42.8 MB (13.89%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -5,15 +5,11 @@
 var runningSum = function(nums) {
   
     const ans = [nums[0]];
-    
-    if (nums.length === 1) {
-        return ans;
-    } else {
-        for(let i=1, j=nums.length; i < j; i++) {
-            ans.push(ans[i-1] + nums[i]);
-        }
-        
-        return ans;
+
+    for(let i=1, j=nums.length; i < j; i++) {
+        ans.push(ans[i-1] + nums[i]);
     }
+        
+    return ans;
      
 };
